51c56121841045d1eb135dd390ba03c73fb49a15
fereshteh_dehghani <89343806+fereshteh-dehghani@users.noreply.github.com>
2022-04-03 10:10:16 +0430

    commit

--- homework-five/draggable-list/script.js
+++ homework-five/draggable-list/script.js
@@ -42,7 +42,63 @@ const listItems=[];
          listItems.push(listItem);
          draggable_List.appendChild(listItem);
 
-     })
+     });
+     addEventListeners();
 
      }
+
+/*Swap function */
+function swapItems(fromIndex,toIndex){
+    const itemOne=listItems[fromIndex].querySelector('.draggable');
+    const itemTwo=listItems[toIndex].querySelector('.draggable');
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+
+
+}
+
+
+/* drag functions */
+function dragStart(){
+    // console.log('Event','dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+    console.log(dragStartIndex);
+}
+function dragEnter(){
+    // console.log('Event','dragEnter');
+    this.classList.add('over');
+}
+function dragOver(){
+    // console.log('Event','dragover');
+}
+function dragLeave(){
+    // console.log('Event','dragLeave');
+    this.classList.remove('over')
+}
+function dragDrop(){
+    // console.log('Event','drop');
+    const dragEndIndex=+this.grtattribute('data-index');
+    swapItems(dragStartIndex,dragEndIndex);
+    this.classList.remove('over');
+}
+
+
+      function addEventListeners(){
+          const draggables=document.querySelectorAll('.draggable');
+          const dragListItems=document.querySelectorAll('.draggable-list li');
+          
+          draggables.forEach(draggable=>{
+              draggable.addEventListener('dragstart',dragStart);
+          });
+           
+          draggables.forEach(item=>{
+            item.addEventListener('dragover',dragOver);
+            item.addEventListener('drop',dragDrop);
+            item.addEventListener('dragenter',dragEnter);
+            item.addEventListener('dragleave',dragLeave);
+        });
+      
+
+
+      }
  
