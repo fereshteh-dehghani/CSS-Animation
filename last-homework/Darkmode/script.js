85fef5441377eebc93269f2311bf4089dec711d2
fereshteh_dehghani <89343806+fereshteh-dehghani@users.noreply.github.com>
2022-04-16 11:07:08 +0430

    end

--- last-homework/Darkmode/script.js
+++ last-homework/Darkmode/script.js
@@ -1,9 +1,9 @@
 const img=document.querySelector('img');
 const dark=document.querySelector('.dark-mode');
 
-img.addEventListener('click',lightMode());
- function lightMode(){
-this.src='./moon-6692.png';
-    document.body.classList.toggle('dark-mode'); 
+img.onclick=
+ () => {
+    document.body.classList.toggle('dark-them'); 
     console.log('darkmode');
+   
  }
